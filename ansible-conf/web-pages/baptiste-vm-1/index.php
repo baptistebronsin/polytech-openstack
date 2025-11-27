87cee734e74f9497d01003286ed8fb1f70468f33
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-11-27 16:00:38 +0100

    first commit

---
+++ ansible-conf/web-pages/baptiste-vm-1/index.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Page PHP de Baptiste 1</title>
+    </head>
+    <body>
+        <h1>Bienvenue sur la page PHP de Baptiste VM 1</h1>
+        <p>Ceci est une page PHP servie par Nginx sur ma machine virtuelle Debian 13.</p>
+        <p>La date et l'heure actuelles sont : <?php echo date('Y-m-d H:i:s'); ?></p>
+    </body>
+</html>
